c5814d6d32ed54e9b6dc508e88cbef975d41d631
Aleksandr Vakhmistrov <dev@vakhmistrov.ru>
2022-05-14 00:59:44 +0300

    Added history api fallback

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require('express');
+const fallback =  require('express-history-api-fallback');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+const root = path.join(__dirname, 'build');
+app.use(express.static(root));
+app.use(fallback('index.html', { root }));
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), function() {
