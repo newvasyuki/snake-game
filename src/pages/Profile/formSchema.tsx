f1d395592aa6092b7c8b71e7462245a48acc3edb
Ren22 <ngm.renat@gmail.com>
2022-05-17 02:37:01 +0200

    sprint 6: updated schema with phone

--- src/pages/Profile/formSchema.tsx
+++ src/pages/Profile/formSchema.tsx
@@ -6,4 +6,5 @@ export const schema = object()
     second_name: string().required('Укажите значение'),
     email: string().email('Укажите email в корректном формате').required('Укажите значение'),
     login: string().required('Укажите значение'),
+    phone: string().required('Укажите значение'), // todo add validation from the chat proj
   });
