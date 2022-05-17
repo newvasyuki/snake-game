c4faff6e4be5ca15336ac7df45f6f2fc815e28ed
Ren22 <ngm.renat@gmail.com>
2022-05-17 13:52:10 +0200

    sprint 6: add registration route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Game } from '../pages/Game';
 import { Profile } from '../pages/Profile';
-import { Layout } from '../components/Layout';
 import { ROUTES } from '../constants';
 import { Forum } from '../pages/Forum';
 import LeaderBoard from '../pages/LeaderBoard/LeaderBoard';
+import { Registration } from '../pages/Registration';
+import { Layout } from '../components/Layout';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route path={ROUTES.rules} element={<h1>Rules</h1>} />
           <Route path={ROUTES.game} element={<Game />} />
         </Route>
+        <Route path={ROUTES.registration} element={<Registration />} />
         <Route path={ROUTES.profile} element={<Profile />} />
         <Route
           path={ROUTES.signIn}
