f4220d6a470ebf74cadcdbc1661a5e6b639bb93d
Ren22 <ngm.renat@gmail.com>
2022-05-17 13:29:49 +0200

    sprint 6: handle 400/500 from fetch

--- src/api/user/UserApi.ts
+++ src/api/user/UserApi.ts
@@ -1,4 +1,5 @@
 import { User } from '../user/types';
+import { UserFormData } from '../../pages/Profile/Profile';
 
 export type PasswordData = {
   oldPassword: string;
@@ -17,29 +18,32 @@ export class UserApi {
       method: 'GET',
       credentials: 'include',
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Get user info failed');
+      })
       .then((userData: User) => userData)
       .catch((error: unknown) => {
         console.error(error);
       });
   }
 
-  changeProfile(userData: User) {
-    return dispatch => {
-      fetch(`${this.baseUrl}/user/profile`, {
-        method: 'PUT',
-        body: JSON.stringify(userData),
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => response.json())
-        .then((data: User) => data)
-        .catch((error: unknown) => {
-          console.log(error);
-        });
-    } 
+  changeProfile(userData: UserFormData) {
+    return fetch(`${this.baseUrl}/user/profile`, {
+      method: 'PUT',
+      body: JSON.stringify(userData),
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data: User) => data)
+      .catch((error: unknown) => {
+        console.log(error);
+      });
   }
 
   changeAvatar(data: FormData) {
@@ -51,11 +55,11 @@ export class UserApi {
       body: data,
       credentials: 'include',
     })
-    .then((response) => response.json())
-    .then((user: User) => user)
-    .catch((error: unknown) => {
-    console.log(error);
-    });
+      .then((response) => response.json())
+      .then((user: User) => user)
+      .catch((error: unknown) => {
+        console.log(error);
+      });
   }
 
   changePassword(data: PasswordData) {
@@ -67,11 +71,11 @@ export class UserApi {
         'Content-Type': 'application/json',
       },
     })
-    // todo: тут скорее всего нужно будет проверять статус ответа
-    .then((response) => response.json())
-    .catch((error: unknown) => {
-    console.log(error);
-    });
+      // todo: тут скорее всего нужно будет проверять статус ответа
+      .then((response) => response.json())
+      .catch((error: unknown) => {
+        console.log(error);
+      });
   }
 }
 
