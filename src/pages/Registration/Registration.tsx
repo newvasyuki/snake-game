279c581e3f489ee70be5f9be0bad3a42e4edd0f9
Renat Nigmetzianov <renat.nigmetzianov@molecularhealth.com>
2022-05-18 18:27:38 +0200

    sprint 6: applies prettier to Registration component

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,37 +1,42 @@
 import React, { useEffect } from 'react';
 import { object, string } from 'yup';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import './Registration.pcss';
-import Input from './components/Input/Input';
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Input/Input';
 import { Button } from '../../components/Button';
 import { registerUser } from '../../store/actionCreators';
 import { useTypedDispatch, useTypedSelector } from '../../store';
 import { ROUTES } from '../../constants';
-import { useNavigate } from 'react-router-dom';
+import { SignUpData } from '../../api';
 
 const schema = object({
   first_name: string().required('Укажите значение'),
   second_name: string().required('Укажите значение'),
   email: string().email('Укажите email').required('Укажите значение'),
   login: string().required('Укажите значение'),
-  password: string().required('Укажите значение').matches(
-    /^(?=.*[A-Z])(?=.*[0-9])[A-Za-z\d]{8,40}$/,
-    "Пароль от 8 до 40 симоволов, содержать число и одну большую букву"
-  ),
-  phone: string().required('Укажите значени е').matches(
-    /^\+?[\d]{10,15}$/,
-    "Допустимый формат +79178383838"
-  )
+  password: string()
+    .required('Укажите значение')
+    .matches(
+      /^(?=.*[A-Z])(?=.*[0-9])[A-Za-z\d]{8,40}$/,
+      'Пароль от 8 до 40 симоволов, содержать число и одну большую букву',
+    ),
+  phone: string()
+    .required('Укажите значени е')
+    .matches(/^\+?[\d]{10,15}$/, 'Допустимый формат +79178383838'),
 }).required();
 
-export default function Registration() {
-
+export const Registration = () => {
   const dispatch = useTypedDispatch();
-  const { isLoggedIn } = useTypedSelector(state => state.auth);
-  const navigate = useNavigate()
-  const { handleSubmit, formState: { errors }, register } = useForm({
+  const { isLoggedIn } = useTypedSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+  } = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
       first_name: '',
@@ -39,85 +44,105 @@ export default function Registration() {
       login: '',
       email: '',
       password: '',
-      phone: ''
+      phone: '',
     },
   });
 
-  const onFormSubmission = async (data) => {
-    console.log(data)
-    dispatch(registerUser(data));
-  }
+  const onFormSubmission = async (data: SignUpData) => {
+    await dispatch(registerUser(data));
+  };
 
   useEffect(() => {
     if (isLoggedIn) {
-      navigate({pathname: ROUTES.profile});
+      navigate({ pathname: ROUTES.profile });
     }
-  }, [isLoggedIn])
+  }, [isLoggedIn]);
 
   return (
-    <div className={'registrationPage'}>
+    <div className="registrationPage">
       <form className="registrationForm" onSubmit={handleSubmit(onFormSubmission)}>
         <div className="registrationForm__container">
           <p className="registrationForm__header">Регистрация</p>
           <div className="registrationForm__inputContainer">
-            <label className="registrationForm__label" htmlFor="first_name">Имя</label>
-            <Input type="text"
+            <label className="registrationForm__label" htmlFor="first_name">
+              Имя
+            </label>
+            <Input
+              type="text"
               className="registrationInputField"
               errorMessage={errors.first_name?.message}
               {...register('first_name')}
             />
           </div>
           <div className="registrationForm__inputContainer">
-            <label className="registrationForm__label" htmlFor="second_name">Фамилия</label>
-            <Input type="text"
+            <label className="registrationForm__label" htmlFor="second_name">
+              Фамилия
+            </label>
+            <Input
+              type="text"
               className="registrationInputField"
               errorMessage={errors.second_name?.message}
               {...register('second_name')}
             />
           </div>
           <div className="registrationForm__inputContainer">
-            <label className="registrationForm__label" htmlFor="login">Логин</label>
-            <Input type="text"
+            <label className="registrationForm__label" htmlFor="login">
+              Логин
+            </label>
+            <Input
+              type="text"
               className="registrationInputField"
               errorMessage={errors.login?.message}
               {...register('login')}
             />
           </div>
           <div className="registrationForm__inputContainer">
-            <label className="registrationForm__label" htmlFor="email">Почта</label>
-            <Input type="text"
+            <label className="registrationForm__label" htmlFor="email">
+              Почта
+            </label>
+            <Input
+              type="text"
               className="registrationInputField"
               errorMessage={errors.email?.message}
               {...register('email')}
             />
           </div>
           <div className="registrationForm__inputContainer">
-            <label className="registrationForm__label" htmlFor="password">Пароль</label>
-            <Input type="password"
+            <label className="registrationForm__label" htmlFor="password">
+              Пароль
+            </label>
+            <Input
+              type="password"
               className="registrationInputField"
               errorMessage={errors.password?.message}
               {...register('password')}
             />
           </div>
           <div className="registrationForm__inputContainer">
-            <label className="registrationForm__label" htmlFor="phone">Телефон</label>
-            <Input type="text"
+            <label className="registrationForm__label" htmlFor="phone">
+              Телефон
+            </label>
+            <Input
+              type="text"
               className="registrationInputField"
               errorMessage={errors.phone?.message}
               {...register('phone')}
             />
           </div>
           <div className="registrationForm_ButtonsContainer">
+            <Button className="registrationForm__defaultButton-colored" type="submit">
+              Зарегистрироваться
+            </Button>
             <Button
-              className='registrationForm__defaultButton-colored'
-              type='submit'
-            >Зарегистрироваться</Button>
-            <Button
-              className='registrationForm__defaultButton-noColor'
-              type='submit'
-            >Войти</Button>
+              className="registrationForm__defaultButton-noColor"
+              onClick={() => navigate({ pathname: ROUTES.signIn })}
+              type="submit"
+            >
+              Войти
+            </Button>
           </div>
         </div>
       </form>
-    </div>)
-}
+    </div>
+  );
+};
