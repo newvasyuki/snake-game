d9c7191e8d17c6dcd4358578bdf30d17f3451446
Ren22 <ngm.renat@gmail.com>
2022-05-23 00:31:09 +0200

    sprint 6: should show user display name in the header

--- src/components/Header/ProfileHelper/ProfileHelper.tsx
+++ src/components/Header/ProfileHelper/ProfileHelper.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './ProfileHelper.pcss';
 import { Link } from 'react-router-dom';
+import { selectUserData } from '../../../store/selectors';
+import { useTypedSelector } from '../../../store';
 
 const ProfileHelper = () => {
+  const userData = useTypedSelector(selectUserData);
+
   return (
     <div className="profile-helper">
       <div className="profile-helper__image" />
       <Link to="/profile">
-        <span className="profile-helper__name">Иван</span>
+        <span className="profile-helper__name">{userData?.display_name}</span>
       </Link>
     </div>
   );
