792b155a3b1aa856fe54958a687a4dbfdf4549f4
Ren22 <ngm.renat@gmail.com>
2022-05-28 16:39:08 +0200

    sprint 7: removes a problem causing TS line

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -3,14 +3,13 @@ import { REGISTER_FAIL, LOGIN_FAILED } from '../actionTypes';
 export type AuthState = {
   isRegistrationFailed: boolean;
   isLoginFailed: boolean;
-  bla: boolean; // have to add this because otherwise TS doesn't see the isLoginFailed
 };
 
 type UserRegisterAction = {
   type: string;
 };
 
-const initialState = { isRegistrationFailed: false, isLoginFailed: false, bla: false };
+const initialState = { isRegistrationFailed: false, isLoginFailed: false };
 
 const authReducer = (state: AuthState = initialState, action: UserRegisterAction) => {
   const { type } = action;
