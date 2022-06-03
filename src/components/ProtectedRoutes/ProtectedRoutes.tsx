4facf6c02d54b706b774a07d5df4a0cff67703ff
Ren22 <ngm.renat@gmail.com>
2022-06-03 14:37:46 +0200

    sprint 7: update routes to use oauth

--- src/components/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../constants';
 import { useTypedDispatch, useTypedSelector } from '../../store';
-import { setUserInfoAsync } from '../../store/actionCreators';
+import { setUserInfoAsync, setUserInfoOAuthAsync } from '../../store/actionCreators';
 
 const ProtectedRoutes = () => {
   const { user, isUserLoading } = useTypedSelector((state) => state.user);
   const dispatch = useTypedDispatch();
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(setUserInfoAsync());
-  }, [dispatch]);
+    const code = searchParams.get('code');
+    if (code) {
+      const redirectUrl = `${window.location.href.split('/').slice(0, -1).join('/')}/`;
+      dispatch(setUserInfoOAuthAsync({ code, redirect_uri: redirectUrl })).then(() => {
+        navigate(ROUTES.game);
+      });
+    } else {
+      dispatch(setUserInfoAsync());
+    }
+  }, [dispatch, navigate, searchParams]);
 
   if (isUserLoading || isUserLoading === undefined || isUserLoading === null) {
     return null;
--- src/components/PublicRoutes/PublicRoutes.tsx
+++ src/components/PublicRoutes/PublicRoutes.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 import { ROUTES } from '../../constants';
 import { useTypedDispatch, useTypedSelector } from '../../store';
-import { setUserInfoAsync } from '../../store/actionCreators';
+import { setUserInfoAsync, setUserInfoOAuthAsync } from '../../store/actionCreators';
 
 const PublicRoutes = () => {
   const { user, isUserLoading } = useTypedSelector((state) => state.user);
   const dispatch = useTypedDispatch();
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    dispatch(setUserInfoAsync());
-  }, [dispatch]);
-
+    const code = searchParams.get('code');
+    if (code) {
+      const redirectUrl = `${window.location.href.split('/').slice(0, -1).join('/')}/`;
+      dispatch(setUserInfoOAuthAsync({ code, redirect_uri: redirectUrl }));
+    } else {
+      dispatch(setUserInfoAsync());
+    }
+  }, [dispatch, searchParams]);
   if (isUserLoading || isUserLoading === undefined || isUserLoading === null) {
     return null;
   }
