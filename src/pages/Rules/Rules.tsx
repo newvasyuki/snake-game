42ae03fe58cb80c89a81acec0c77f41ab91640d9
Aleksandr Vakhmistrov <dev@vakhmistrov.ru>
2022-06-06 18:28:37 +0300

    Updated

--- src/pages/Rules/Rules.tsx
+++ src/pages/Rules/Rules.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import bemCn from 'bem-cn-lite';
 import './Rules.pcss';
 
+const block = bemCn('rules-page');
+
 const Rules = () => {
-  return
+  return (
+    <div className={block()}>
+      <h3>Правило №1</h3>
+      <p>Никогда не нажимать Pause!</p>
+      <h3>Правило №1</h3>
+      <p>Никогда не нажимать Pause!</p>
+      <h3>Правило №1</h3>
+      <p>Никогда не нажимать Pause!</p>
+      <h3>Правило №1</h3>
+      <p>Никогда не нажимать Pause!</p>
+    </div>
+  )
 }
 
 export default Rules;
