bbff39a8d1f79a35220edf4b1c63f3ec7346b13c
Самотрясов Роман Русланович <rsamotryasov@t1-consulting.ru>
2022-06-17 22:29:56 +0300

    Доабавляет тесты для инпута

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,43 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
+import Input from './Input';
+
+afterEach(cleanup);
+
+describe('Тестирование компонента Input', () => {
+  test('Проверяет отрисовку инпута', () => {
+    render(<Input />);
+
+    const input = screen.getByTestId('input');
+
+    expect(input).toBeInTheDocument();
+  });
+
+  test('Проверяет наличие и значение атрибута type у поля ввода', () => {
+    render(<Input type="text" />);
+
+    const inputField = screen.getByTestId('input-control');
+
+    expect(inputField).toBeInTheDocument();
+    expect((inputField as HTMLInputElement).type).toBe('text');
+  });
+
+  test('Проверяет работу события onChange на инпуте', () => {
+    render(<Input />);
+
+    const inputField = screen.getByTestId('input-control');
+
+    fireEvent.change(inputField, { target: { value: 'test test' } });
+    expect(inputField).toBeInTheDocument();
+    expect((inputField as HTMLInputElement).value).toBe('test test');
+  });
+
+  test('Проверяет отрисовку ошибки в компоненте инпута', () => {
+    render(<Input errorMessage="Test error message" />);
+
+    const errorElement = screen.getByText('Test error message');
+
+    expect(errorElement).toBeInTheDocument();
+  });
+});
