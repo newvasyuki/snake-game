a5950ed55fcb549b49c67cd108e8aec784e80dce
Самотрясов Роман Русланович <rsamotryasov@t1-consulting.ru>
2022-06-22 23:16:42 +0300

    Начал создавать модалку для создания темы форума

--- src/pages/Forum/AddButton/AddButton.tsx
+++ src/pages/Forum/AddButton/AddButton.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import bemCn from 'bem-cn-lite';
 import PlusIcon from '../../../../assets/plus-icon.react.svg';
 import './AddButton.pcss';
 
 const block = bemCn('add-button');
 
-export const AddButton = () => {
+type Props = Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'>;
+
+export const AddButton: React.FC<Props> = ({ onClick }) => {
   return (
-    <button type="button" aria-label="Добавить тему" className={block()}>
+    <button type="button" aria-label="Добавить тему" className={block()} onClick={onClick}>
       <div className={block('icon-wrapper')}>
         <PlusIcon />
       </div>
--- src/pages/Forum/Header/Header.tsx
+++ src/pages/Forum/Header/Header.tsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { MouseEvent, ReactNode, useState } from 'react';
+import ReactDOM from 'react-dom';
 import bemCn from 'bem-cn-lite';
+import { Input } from 'components/Input';
+import { Button } from 'components/Button';
 import { Navigation } from '../Navigation';
 import { AddButton } from '../AddButton';
 import './Header.pcss';
 
 const block = bemCn('forum-header');
 
+const topicBlock = bemCn('create-topic');
+
+const CreateTopicForm = ({ onCancel }) => {
+  return (
+    <div className={topicBlock()}>
+      <Input />
+      <textarea />
+      <Button>Создать</Button>
+      <Button onClick={onCancel}>Отмена</Button>
+    </div>
+  );
+};
+
+type ModalProps = React.PropsWithChildren<{
+  container: Element;
+  onClose?: () => void;
+  isOpen: boolean;
+}>;
+
+const Modal: React.FC<ModalProps> = ({ children, container, isOpen, onClose }) => {
+  const wrappedChildren = (
+    <div className="modal-mask">
+      <div className="modal-wrapper">{children}</div>
+    </div>
+  );
+  return isOpen ? ReactDOM.createPortal(wrappedChildren, container) : null;
+};
+
+// type Props = {
+//   isOpened: boolean;
+//   onClose?: () => void;
+// };
+
+// const CreateTopicModal: React.FC<Props> = ({ isOpened, onClose }) => {
+//   return (
+//     <Modal isOpen={isOpened} container={document.getElementById('root')} onClose={onClose}>
+//       <CreateTopicForm onCancel={onClose} />
+//     </Modal>
+//   );
+// };
+
 export const Header = () => {
+  const [isCreatedTopic, setIsCreatedTopic] = useState(false);
+
+  const onButtonClick = () => {
+    setIsCreatedTopic((prevState) => !prevState);
+  };
+
   return (
     <div className={block()}>
       <Navigation />
-      <AddButton />
+      <AddButton onClick={onButtonClick} />
+      {/* <CreateTopicModal isOpened={isCreatedTopic} onClose={() => setIsCreatedTopic(false)} /> */}
+      <Modal
+        isOpen={isCreatedTopic}
+        container={document.getElementById('root')}
+        onClose={() => setIsCreatedTopic(false)}
+      >
+        <CreateTopicForm onCancel={() => setIsCreatedTopic(false)} />
+      </Modal>
     </div>
   );
 };
