fde797418aa9db915360a124e31df5683260f7ab
Ren22 <ngm.renat@gmail.com>
2022-06-24 14:06:15 +0200

    sprint 8: adds forum API

--- server/api/routes/apiRouter.ts
+++ server/api/routes/apiRouter.ts
@@ -1,4 +1,4 @@
-import { Router, Response, Request } from 'express';
+import { Router } from 'express';
 import { getComments } from '../collections/comments/get';
 import { postComment } from '../collections/comments/post';
 import { getTopics } from '../collections/topics/get';
@@ -6,16 +6,9 @@ import { postTopic } from '../collections/topics/post';
 
 const API_V1 = '/api/v1';
 
-const getApiDocumentation = (req: Request, res: Response) => {
-  res
-    .status(200)
-    .send(['api/v1/forum/topics', 'api/v1/forum/topics/{id}', 'api/v1/forum/topics/{id}/comments']);
-};
-
 export function configureApiRouter() {
   const router: Router = Router();
 
-  router.get(`${API_V1}`, getApiDocumentation);
   router.get(`${API_V1}/forum/topics`, getTopics);
   router.get(`${API_V1}/forum/topics/:id(\\d+)/comments`, getComments);
   router.post(`${API_V1}/forum/topics`, postTopic);
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import path from 'path';
 import express from 'express';
+import swaggerUi, { JsonObject } from 'swagger-ui-express';
 import render from './render';
 import { dbConnect } from '../db/init';
 import { configureApiRouter } from './api/routes/apiRouter';
+import swaggerDoc from '../swagger.json';
 
 const app = express();
 
@@ -11,6 +13,7 @@ const app = express();
   await dbConnect();
 })();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc as JsonObject));
 app.use(express.static(path.resolve(__dirname, '../../')));
 app.use(express.json());
 app.use(configureApiRouter(), []); // todo: create safety net for 404, or 500
