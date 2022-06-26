2e9278e45ef6eefe3468c882f69a6612e94851a5
Ren22 <ngm.renat@gmail.com>
2022-06-27 01:22:43 +0200

    sprint 8: remove cors

--- server/api/routes/apiRouter.ts
+++ server/api/routes/apiRouter.ts
@@ -3,16 +3,17 @@ import { getComments } from '../collections/comments/get';
 import { postComment } from '../collections/comments/post';
 import { getTopics } from '../collections/topics/get';
 import { postTopic } from '../collections/topics/post';
+import { authorizeUser } from '../middlewares/auth';
 
 const API_V1 = '/api/v1';
 
 export function configureApiRouter() {
   const router: Router = Router();
 
-  router.get(`${API_V1}/forum/topics`, getTopics);
-  router.post(`${API_V1}/forum/topics`, postTopic);
-  router.get(`${API_V1}/forum/topics/:id(\\d+)/comments`, getComments);
-  router.post(`${API_V1}/forum/topics/:id(\\d+)/comments`, postComment);
+  router.get(`${API_V1}/forum/topics`, [authorizeUser], getTopics);
+  router.post(`${API_V1}/forum/topics`, [authorizeUser], postTopic);
+  router.get(`${API_V1}/forum/topics/:id(\\d+)/comments`, [authorizeUser], getComments);
+  router.post(`${API_V1}/forum/topics/:id(\\d+)/comments`, [authorizeUser], postComment);
 
   return router;
 }
