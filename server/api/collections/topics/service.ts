ada1929c15468e0bc58f0392a9cda5f508e0216d
Ren22 <ngm.renat@gmail.com>
2022-06-30 12:39:22 +0200

    sprint 9: resolves comments for topics in the backend

--- server/api/collections/topics/service.ts
+++ server/api/collections/topics/service.ts
@@ -1,8 +1,9 @@
 import { Topic } from '../../../../db/models/topic';
 import { TopicInput } from './types';
+import { Comment } from '../../../../db/models/comment';
 
 export const loadTopics = async () => {
-  return Topic.findAll();
+  return Topic.findAll({ include: [Comment] });
 };
 
 export const saveTopicToDb = async (topic: TopicInput, userId: number) => {
