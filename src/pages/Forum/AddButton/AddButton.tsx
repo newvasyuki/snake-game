ee5ef5552478cccdbc1dc108a9429c250776630b
Ren22 <ngm.renat@gmail.com>
2022-07-08 02:39:11 +0200

    sprint 9: updates topics after creation

--- src/pages/Forum/AddButton/AddButton.tsx
+++ src/pages/Forum/AddButton/AddButton.tsx
@@ -4,17 +4,27 @@ import bemCn from 'bem-cn-lite';
 import './AddButton.pcss';
 import { createForumTopic } from 'api/forum';
 import randomWords from 'random-words';
+import { useTypedDispatch, useTypedSelector } from 'store';
+import { selectUserData } from 'store/selectors';
+import { setThreads } from 'store/actionCreators';
 import PlusIcon from '../../../../assets/plus-icon.react.svg';
 
 const block = bemCn('add-button');
 
 export const AddButton = () => {
+  const user = useTypedSelector(selectUserData);
+  const dispatch = useTypedDispatch();
+
   const onAddTopic = async () => {
     try {
-      await createForumTopic({
-        title: randomWords(2).join(' '),
-        content: randomWords(5).join(' '),
-      });
+      await createForumTopic(
+        {
+          title: randomWords(2).join(' '),
+          content: randomWords(5).join(' '),
+        },
+        user.id,
+      );
+      dispatch(setThreads());
     } catch (e) {
       console.error(e);
     }
