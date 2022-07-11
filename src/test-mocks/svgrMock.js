353ec87aaa0ec8e0dcf011b60f366855465edc16
Ren22 <ngm.renat@gmail.com>
2022-07-11 02:32:53 +0200

    sprint 9: fix to test

--- src/test-mocks/svgrMock.js
+++ src/test-mocks/svgrMock.js
@@ -1,2 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
 export default 'SvgrURL';
-export const ReactComponent = 'div';
+const SvgrMock = React.forwardRef((props, ref) => <span ref={ref} {...props} />);
+
+export const ReactComponent = SvgrMock;
